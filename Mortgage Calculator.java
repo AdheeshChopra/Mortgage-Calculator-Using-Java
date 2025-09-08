de8f62626c0e9975b334bba7160247b688514559
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-09-08 18:49:10 +0530

    Update Mortgage Calculator.java

--- Mortgage Calculator.java
+++ Mortgage Calculator.java
@@ -2,23 +2,43 @@ package MortgageCalculator;
 import java.util.*;
 import java.text.*;
 
-public class Calculator {
+public class MortgageCalculator {
     public static void main(String[] args){
         int MonthsInYear=12;
         int Percent=100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
         Scanner sc=new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = sc.nextInt();
+        while(true) {
+            System.out.print("Principal: ");
+            principal = sc.nextInt();
+            if(principal>=1000 && principal<=1000000) break;
+            System.out.print("Enter a valid value between 1000 and 1000000: ");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest=sc.nextFloat();
-        float monthlyInterest=annualInterest/Percent/MonthsInYear;
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = sc.nextFloat();
+            if(annualInterest>=1 && annualInterest<=30){
+                monthlyInterest = annualInterest / Percent / MonthsInYear;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("Period(Years): ");
-        int years=sc.nextInt();
-        int numberOfPayments = years*MonthsInYear;
+        while(true) {
+            System.out.print("Period(Years): ");
+            int years = sc.nextInt();
+            if(years>=1 && years<=30) {
+                numberOfPayments = years * MonthsInYear;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1+monthlyInterest, numberOfPayments))
